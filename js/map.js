50bd11f9742e98e56b4a7fabe1dae36243581105
brainiac <brainiac2k@gmail.com>
2015-01-09 00:27:46 -0800

    More map functionality

--- js/map.js
+++ js/map.js
@@ -1,3 +1,5 @@
+var SCALE_FACTOR = 1000.0;
+
 function componentToHex(c) {
     c = typeof(c) === "string" ? parseInt(c) : c;
     var hex = c.toString(16);
@@ -10,20 +12,98 @@ function rgbToHex(r, g, b) {
 
 function createPoint(x, y) {
     return L.latLng(
-        (typeof(x) === "string" ? parseFloat(x) : x) / 1000.0,
-        (typeof(y) === "string" ? parseFloat(y) : y) / 1000.0);
+        (typeof(x) === "string" ? parseFloat(x) : x) / SCALE_FACTOR,
+        (typeof(y) === "string" ? parseFloat(y) : y) / SCALE_FACTOR);
 }
+
+function createLabel(point, text) {
+    text = text.replace("_", " ");
+   var icon = L.divIcon({
+        className: 'point-circle',
+        html: text,
+        iconSize: [2, 2]
+    });
+
+    return L.marker(point, {icon: icon, title: text});
+}
+
+function createGridLayer() {
+    var lines = [];
+    var d = 2, p = .1;
+    for (var x = -d; x <= d + p; x += p) {
+        lines.push([L.latLng(x, -d), L.latLng(x, d)]);
+        lines.push([L.latLng(-d, x), L.latLng(d, x)]);
+    }
+    return L.multiPolyline(lines, {
+        color: "#777",
+        opacity: 0.5,
+        weight: 1
+    });
+}
+
+L.Control.Loader = L.Control.extend({
+    onAdd: function(map) {
+        this._map = map;
+        this._container = L.DomUtil.create('div','leaflet-control-loader');
+        this.hide();
+        return this._container;
+    },
+    addTo: function(map) {
+        this._container = this.onAdd(map);
+        map.getContainer().appendChild(this._container);
+        return this;
+    },
+    show: function() {
+        this._container.style.display = 'block';
+        return this;
+    },
+    hide: function() {
+        this._container.style.display = 'none';
+        return this;
+    }
+});
+
+L.Map.addInitHook(function () {
+    if (this.options.loaderControl) {
+        this.loaderControl = L.control.loader(this.options.loaderControl);
+        this.addControl(this.loaderControl);
+    }
+});
+
+L.control.loader = function(options) {
+    return new L.Control.Loader(options);
+};
 
 //------------------------------------------------------------------------------
 
 var MapObject = function(mapId) {
-    this.map = L.map(mapId);
+    this.map = L.map(mapId, {crs: L.CRS.Simple});
     this.layers = [];
+
+    L.control.mousePosition({
+        separator: ", ",
+        latFormatter: function(l) { return (l*SCALE_FACTOR).toFixed(2) },
+        lngFormatter: function(l) { return (l*SCALE_FACTOR).toFixed(2) }
+    }).addTo(this.map);
+    
+    // Create grid and layer control
+    var gridLayer = createGridLayer();
+    gridLayer.addTo(this.map);
+
+    gridLayer = {
+        "Grid": gridLayer
+    };
+    L.control.layers([], gridLayer, {
+        collapsed: false
+    }).addTo(this.map);
+
+    this.loaderControl = L.control.loader().addTo(this.map);
 }
 
 MapObject.prototype.fitView = function() {
     this.map.fitBounds(L.latLngBounds(
-        L.latLng(-2, -2), L.latLng(1, 2)));
+        L.latLng(-2000 / SCALE_FACTOR, -2000 / SCALE_FACTOR),
+        L.latLng( 1000 / SCALE_FACTOR,  2000 / SCALE_FACTOR)));
     this.map.setZoom(9);
 }
 
@@ -31,8 +111,33 @@ MapObject.prototype.getMap = function() {
     return this.map;
 }
 
+MapObject.prototype.addMapLayers = function(layers) {
+    // layers is a dictionary of names and layers
+    if (this.layersControl) {
+        this.map.removeControl(this.layersControl);
+    }
+    this.layersControl = null;
+
+    for (var l in this.layers) {
+        this.map.removeLayer(l);
+    }
+    this.layers = [];
+
+    for (var layerName in layers) {
+        var layer = layers[layerName];
+        if (layerName === "0")
+            this.map.addLayer(layer);
+        this.layers.push(layer);
+    }
+
+    this.layersControl = L.control.layers([], layers, {collapsed: false});
+    this.layersControl.addTo(this.map);
+}
+
 MapObject.prototype.loadMapLayer = function(filename) {
     var obj = this;
+
+    // todo: remove all map layers
 
     // creates a layerGroup given the contents from the map filename
     processMapData = function(data) {
@@ -40,6 +145,10 @@ MapObject.prototype.loadMapLayer = function(filename) {
 
         // lines grouped by color
         var linesByColor = {};
+
+        if (data.length === 0) {
+            return layerGroup;
+        }
 
         // iterate over lines, parse each for a line or a point label
         var linesText = data.match(/[^\r\n]+/g);
@@ -61,14 +170,8 @@ MapObject.prototype.loadMapLayer = function(filename) {
                 linesByColor[c].push(p);
             }
             else if (pieces[0] == 'P') {
-                // a point label
-                /*
-                var p = L.latLng(parseFloat(pieces[1]) / 1000.0, parseFloat(pieces[2]) / 1000.0);
-
-                var l = new L.Label()
-                l.setcontent(pieces[8]);
-                l.setLatLng(p);
-                map.showLabel(l);*/
+                var label = createLabel(createPoint(pieces[1], pieces[2]), pieces[8]);
+                layerGroup.addLayer(label);
             }
         }
 
@@ -78,24 +181,56 @@ MapObject.prototype.loadMapLayer = function(filename) {
                 opacity: 1.0,
                 weight: 1
             });
-            //layerGroup.addLayer(lines);
-
-            lines.addTo(obj.map);
+            layerGroup.addLayer(lines);
         }
 
-        //layerGroup.addTo(obj.map);
+        return layerGroup;
     }
 
-    $.ajax({
-        type: 'GET',
-        url: 'maps/' + filename + '.txt',
-        dataType: 'text',
-        success: processMapData,
-        error: function(xhr, status, errorThrown) {
-            console.log("failed to fetch map: " + filename);
-            console.dir([xhr, status, errorThrown]);
+    // each map has a base and 3 layers
+    maps = {"0": filename,
+        "1": filename + "_1",
+        "2": filename + "_2",
+        "3": filename + "_3"
+    };
+    var completeCb = {
+        layers: {},
+        object: obj,
+        remaining: 4,
+
+        complete: function() {
+            --this.remaining;
+            if (this.remaining == 0) {
+                console.log("done!");
+                this.object.loaderControl.hide();
+                this.object.addMapLayers(this.layers);
+            }
         }
-    })
+    };
+
+    this.loaderControl.show();
+
+    for (var k in maps) {
+        var success = (function(k, v) {
+            return function(data) {
+                var layerGroup = processMapData(data);
+
+                completeCb.layers[k] = layerGroup;
+                completeCb.complete();
+            };
+        })(k, maps[k]);
+
+        $.ajax({
+            type: 'GET',
+            url: 'maps/' + maps[k] + '.txt',
+            dataType: 'text',
+            success: success,
+            error: function(xhr, status, errorThrown) {
+                console.dir([xhr, status, errorThrown]);
+                completeCb.complete();
+            }
+        });
+    }
 }
 
 var theMap;
